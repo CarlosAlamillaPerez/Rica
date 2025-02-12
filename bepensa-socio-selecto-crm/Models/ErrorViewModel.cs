3afec1f9544d6db8f4cc88850d0ce76bbf4820f4
Manuel Flores <jose.flores@mx.lms-la.com>
2025-02-12 11:47:23 -0600

    Inicio de proyecto

---
+++ bepensa-socio-selecto-crm/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace bepensa_socio_selecto_crm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ bepensa-socio-selecto-web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace bepensa_socio_selecto_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
