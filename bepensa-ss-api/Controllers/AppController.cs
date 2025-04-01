69d1645cd03e9595705c0b40358f845641c276e5
sigonzalez <sigonzalez@lms-la.com>
2025-04-01 16:59:28 -0600

    métodos para imágenes

---
+++ bepensa-ss-api/Controllers/AppController.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using bepensa_biz.Interfaces;
+using bepensa_models.DTO;
+using bepensa_models.Enums;
+using bepensa_models.General;
+using bepensa_models.DataModels;
+using bepensa_biz;
+using System.Threading.Tasks;
+
+namespace bepensa_ss_api;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+public class AppController : ControllerBase
+{
+    private readonly IMapper mapper;
+    private readonly ISecurity _security;
+    private readonly IApp _app;
+
+    public AppController(IMapper mapper, ISecurity security, IApp app)
+    {
+        this.mapper = mapper;
+        _security = security;
+        _app = app;
+    }
+
+    [HttpPost("ConsultaParametro")]
+    public async Task<ActionResult<Respuesta<string>>> ConsultaParametro(int pParametro)
+    {
+        Respuesta<string> resultado = new();
+
+        try
+        {
+            resultado = await _app.ConsultaParametro(pParametro);
+
+            return Ok(resultado);
+        }
+        catch (Exception)
+        {
+            resultado.Exitoso = false;
+            resultado.Codigo = (int)CodigoDeError.Excepcion;
+            resultado.Data = null;
+            resultado.Mensaje = CodigoDeError.Excepcion.GetDescription();
+
+            return BadRequest(resultado);
+        }
+    }
+
+}
