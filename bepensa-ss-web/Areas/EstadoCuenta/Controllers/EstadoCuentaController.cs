a395212b082eddd89a5b9df87cb1b32b525eac79
Manuel Flores <jose.flores@mx.lms-la.com>
2025-04-01 18:06:18 -0600

    Se agrega sección de estado de cuenta.

---
+++ bepensa-ss-web/Areas/EstadoCuenta/Controllers/EstadoCuentaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bepensa_ss_web.Areas.EstadoCuenta.Controllers
+{
+    [Area("EstadoCuenta")]
+    public class EstadoCuentaController : Controller
+    {
+        [HttpGet("estado-de-cuenta")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
