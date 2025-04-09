cbc1466dd5809246dc4bcefcf701b7e2f8e01fa6
Manuel Flores <jose.flores@mx.lms-la.com>
2025-04-09 11:11:09 -0600

    servicio de estado de cuenta.

---
+++ bepensa-ss-api/Controllers/EstadosCuentaController.cs
@@ -0,0 +1,20 @@
+﻿using bepensa_biz.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bepensa_ss_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstadosCuentaController : ControllerBase
+    {
+        private readonly IEstadoCuenta _estadoCuenta;
+
+        public EstadosCuentaController(IEstadoCuenta estadoCuenta)
+        {
+            _estadoCuenta = estadoCuenta;
+        }
+
+
+    }
+}
