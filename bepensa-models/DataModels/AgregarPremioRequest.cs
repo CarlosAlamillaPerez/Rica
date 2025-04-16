2d704a21ad9009bce3e61da259fb836bf15a4a82
Manuel Flores <jose.flores@mx.lms-la.com>
2025-04-15 18:22:41 -0600

    Se añade métodos para agregar carrito y consultar carrito, no se ha probado.

---
+++ bepensa-models/DataModels/AgregarPremioRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bepensa_models.DataModels
+{
+    public class AgregarPremioRequest
+    {
+        [Display(Name = "IdUsuario")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Usuario inválido.")]
+        public int IdUsuario { get; set; }
+
+        [Display(Name = "IdPremio")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Premio inválido.")]
+        public int IdPremio { get; set; }
+
+        [Display(Name = "Cantidad")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Cantidad inválida.")]
+        public int Cantidad { get; set; } = 1;
+
+        //[Display(Name = "Telefono")]
+        //[RegularExpression("(^[0-9]+$)", ErrorMessage = "El campo {0} solo permite números")]
+        //[MinLength(10, ErrorMessage = "El campo {0} debe contener 10 caracteres")]
+        //[MaxLength(10, ErrorMessage = "El campo {0} debe contener 10 caracteres")]
+        //public string? TelefonoRecarga { get; set; } = null;
+    }
+}
---
+++ bepensa-ss-api/Controllers/CarritoController.cs
@@ -0,0 +1,47 @@
+﻿using bepensa_biz.Interfaces;
+using bepensa_models.DataModels;
+using bepensa_models.DTO;
+using bepensa_models.Enums;
+using bepensa_models.General;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bepensa_ss_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarritoController : ControllerBase
+    {
+        private readonly ICarrito _carrito;
+
+        public CarritoController(ICarrito carrito)
+        {
+            _carrito = carrito;
+        }
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpPost("AgregarPremio")]
+        public ActionResult<Respuesta<Empty>> ConsultarMeteMensual(AgregarPremioRequest pUsuario)
+        {
+            Respuesta<Empty> resultado = new();
+
+            try
+            {
+                resultado = _carrito.AgregarPremio(pUsuario);
+
+                return Ok(resultado);
+            }
+            catch (Exception)
+            {
+                resultado.Exitoso = false;
+                resultado.Codigo = (int)CodigoDeError.Excepcion;
+                resultado.Data = null;
+                resultado.Mensaje = CodigoDeError.Excepcion.GetDescription();
+
+                return BadRequest(resultado);
+            }
+        }
+    }
+}
