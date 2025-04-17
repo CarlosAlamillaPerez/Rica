74c0950ed815a268a145eff3ad0750161869d41b
Manuel Flores <jose.flores@mx.lms-la.com>
2025-04-17 12:39:27 -0600

    Se añade color para OP

---
+++ bepensa-ss-web/Areas/Socio/Controllers/CarritoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bepensa_ss_web.Areas.Socio.Controllers
+{
+    [Area("Socio")]
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+    public class CarritoController : Controller
+    {
+        [HttpGet("carrito")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
