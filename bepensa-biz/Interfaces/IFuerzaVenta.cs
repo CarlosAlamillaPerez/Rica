b929d5142dd26590f1208924b9c12255cb93e53c
Manuel Flores <jose.flores@mx.lms-la.com>
2025-05-07 16:24:18 -0600

    Se agrega IdOrigen para control de destino.

--- bepensa-biz/Interfaces/IFuerzaVenta.cs
+++ bepensa-biz/Interfaces/IFuerzaVenta.cs
@@ -1,13 +1,14 @@
 ﻿using bepensa_models.App;
 using bepensa_models.DataModels;
 using bepensa_models.DTO;
+using bepensa_models.Enums;
 using bepensa_models.General;
 
 namespace bepensa_biz.Interfaces
 {
     public interface IFuerzaVenta
     {
-        Task<Respuesta<FuerzaVentaDTO>> ValidaAcceso(LoginApp credenciales, int idCanal);
+        Task<Respuesta<FuerzaVentaDTO>> ValidaAcceso(LoginApp credenciales, int idCanal, int idOrigen = (int)TipoOrigen.App);
 
         Task<Respuesta<List<UsuarioDTO>>> ConsultarUsuarios(BuscarFDVRequest pBuscar);
 
