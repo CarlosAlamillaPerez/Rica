586ac1a496b4cc3e89d2cd22536432891467a928
Manuel Flores <jose.flores@mx.lms-la.com>
2025-05-09 13:21:11 -0600

    Se agregan promociones al CRM.

---
+++ bepensa-ss-crm/Areas/Usuario/Controllers/PromocionesController.cs
@@ -0,0 +1,28 @@
+﻿using bepensa_biz.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bepensa_ss_crm.Areas.Usuario.Controllers
+{
+    [Area("Usuario")]
+    [Authorize]
+    public class PromocionesController : Controller
+    {
+        private readonly IAccessSession _sesion;
+        private readonly IApp _app;
+
+        public PromocionesController(IAccessSession sesion, IApp app)
+        {
+            _sesion = sesion;
+            _app = app;
+        }
+
+        [HttpGet("promociones")]
+        public async Task<IActionResult> Index()
+        {
+            var resultado = await _app.ConsultaImgPromociones(_sesion.UsuarioActual.IdCanal);
+
+            return View(resultado.Data);
+        }
+    }
+}
