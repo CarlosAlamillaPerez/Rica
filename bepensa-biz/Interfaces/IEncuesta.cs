11b5f6c3d8a8389e3658edc4b272239a58881dce
Manuel Flores <jose.flores@mx.lms-la.com>
2025-05-22 16:46:12 -0600

    servicio de encuesta

---
+++ bepensa-biz/Interfaces/IEncuesta.cs
@@ -0,0 +1,10 @@
+﻿using bepensa_data.models;
+using bepensa_models.General;
+
+namespace bepensa_biz.Interfaces
+{
+    public interface IEncuesta
+    {
+        Respuesta<List<Encuesta>> ConsultarEncuestas(int pIdUsuario);
+    }
+}
