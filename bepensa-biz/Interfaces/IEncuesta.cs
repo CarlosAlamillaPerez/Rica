684e2064999a434431aa7c491e58e8257d2b12c7
Manuel Flores <jose.flores@mx.lms-la.com>
2025-05-22 16:56:29 -0600

    estructura para servicio de guardado de encuesta

--- bepensa-biz/Interfaces/IEncuesta.cs
+++ bepensa-biz/Interfaces/IEncuesta.cs
@@ -7,5 +7,7 @@ namespace bepensa_biz.Interfaces
     public interface IEncuesta
     {
         Respuesta<List<BitacoraEncuestaDTO>> ConsultarEncuestas(int pIdUsuario);
+
+        Respuesta<Empty> ResponderEncuesta(EncuestaRequest pEncuesta);
     }
 }
--- bepensa-models/DataModels/RepuestaEncuestaRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace bepensa_models.DataModels;
-
-public class RepuestaEncuestaRequest
-{
-    public int IdBitacoraEncuesta { get; set; }
-
-    public int IdPregunta { get; set; }
-}
