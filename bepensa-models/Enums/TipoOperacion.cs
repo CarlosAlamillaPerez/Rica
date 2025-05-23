1747da5a9a0756ed019dcdbad08831ece92f248d
Manuel Flores <jose.flores@mx.lms-la.com>
2025-05-23 13:19:33 -0600

    Se agrega bitacora para servicio de respuesta de encuesta en biss

--- bepensa-models/Enums/TipoOperacion.cs
+++ bepensa-models/Enums/TipoOperacion.cs
@@ -31,9 +31,19 @@ public enum TipoOperacion
     [Display(Name = "Modifico cantidad premio a carrito", Description = "Modifico cantidad premio a carrito")]
     ModificaCarrito = 9,
 
+    [Display(Name = "Modifico cantidad premio a carrito", Description = "Modifico cantidad premio a carrito")]
+    ProcesarCarrito = 10,
+
+    [Display(Name = "Fuerza de Venta inicia sesión en usuario", Description = "Fuerza de Venta inicia sesión en usuario")]
+    InicioSesionFDV = 11,
+
     [Display(Name = "Registro de llamada", Description = "Registro de llamada")]
     RegistroLlamada = 12,
 
     [Display(Name = "Elimino carrito", Description = "Elimino carrito")]
-    EliminoCarrito = 13
+    EliminoCarrito = 13,
+
+    [Display(Name = "Encuesta completada", Description = "Encuesta completada")]
+    EncuestaCompletada = 14
+
 }
