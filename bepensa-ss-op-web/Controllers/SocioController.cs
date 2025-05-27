a3eb914d9e8ee6b00c7ed297fc4cff1a8465a38d
Manuel Flores <jose.flores@mx.lms-la.com>
2025-05-27 15:50:13 -0600

    Se añade landing FDV OP

---
+++ bepensa-ss-op-web/Controllers/SocioController.cs
@@ -0,0 +1,50 @@
+﻿using bepensa_biz.Interfaces;
+using bepensa_biz.Security;
+using bepensa_models.DataModels;
+using bepensa_models.DTO;
+using bepensa_models.Enums;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bepensa_ss_op_web.Controllers
+{
+    public class SocioController : Controller
+    {
+        private readonly IConfiguration configuration;
+        private readonly IObjetivo _objetivo;
+
+        public SocioController(IConfiguration configuration, IObjetivo objetivo)
+        {
+            this.configuration = configuration;
+            _objetivo = objetivo;
+        }
+
+
+        [HttpGet("landing/fdv/{pCuc}/{pToken}")]
+        public IActionResult Index(string pCuc, Guid pToken)
+        {
+            Hash hash = new(pToken.ToString());
+
+            var _token = hash.Sha512();
+
+            var token = Convert.ToHexString(_token);
+
+            if (token == null || !(token == configuration.GetValue<string>("KeySocioSelecto")))
+            {
+                TempData["msgError"] = CodigoDeError.SesionCaducada.GetDescription();
+
+                return RedirectToAction("Login", "Cuentas", new { area = "Autenticacion" });
+            }
+
+            var modelo = new ResumenSocioSelectoDTO();
+
+            var resultado = _objetivo.ResumenSocioSelecto(new LandingFDVRequest { Cuc = pCuc });
+
+            if (resultado.Data != null)
+            {
+                modelo = resultado.Data;
+            }
+
+            return View(modelo);
+        }
+    }
+}
