76214ec12a78308b263c551f11785fecd2772600
Manuel Flores <jose.flores@mx.lms-la.com>
2025-06-02 16:30:48 -0600

    Se crea método para registrar en JsonElement del Logger.

--- bepensa-biz/Proxies/ProxyBase.cs
+++ bepensa-biz/Proxies/ProxyBase.cs
@@ -1,8 +1,11 @@
 ﻿using bepensa_data.data;
+using bepensa_data.logger.data;
 
 namespace bepensa_biz.Proxies;
 
 public abstract class ProxyBase
 {
     protected BepensaContext DBContext { get; set; } = null!;
+
+    protected BepensaLoggerContext LoggerContext { get; set; } = null!;
 }
